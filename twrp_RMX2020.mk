37328a90cea3162428ccc2ec9769fb8496365b58
Koushik Dey <kousickdey3@gmail.com>
2024-02-27 00:00:53 +0530

    RMX2020: Start health hal
    443 W libc    : Unable to set property "ctl.interface_start" to "android.hardware.health@2.0::IHealth/default": error code: 0x20
    	Line 2177: 02-17 14:40:41.938   321   443 I hwservicemanager: Tried to start android.hardware.health@2.0::IHealth/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.
    
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>

--- twrp_RMX2020.mk
+++ twrp_RMX2020.mk
@@ -40,3 +40,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
